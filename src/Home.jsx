653e6ecaeb8557a2a7943082e5512414b1de1d67
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-07-12 13:35:39 +0200

    another commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 
 const Home = () => {
+  useEffect(() => {
+    const images = document.querySelectorAll(".social-media img");
+    images.forEach((img) => {
+      img.style.width= "20px"
+      img.style.height= "20px"
+    })
+  }, [])
+  
+ 
   return (
-    <div className="flex bg-home h-screen bg-cover bg-center p-40 opacity-40">
-      <div className="flex-col ">
-        <div className = "flex text-6xl font-bold pb-4 ">Tech Enthusiast</div>
-        <div className="text-sky-800 text-2xl font-thin">Full-Stack Programmer</div>
-        <div className="flex space-x-4 mt-4">
+    <div className="relative flex bg-home h-screen bg-cover bg-center p-40 ">
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          backgroundColor: "white",
+          opacity:0.7,
+          zIndex: 0,
+        }}
+      ></div>
+      <div className="z-10 flex-col ">
+        <div className="flex text-6xl font-bold pb-4 ">Tech Enthusiast</div>
+        <div className="text-sky-800 text-2xl font-normal
+        social-media">
+          Full-Stack Programmer
+        </div>
+        <div className="flex space-x-4 mt-4" style={{
+          img
+        }}>
           <div>
             <a href="https://www.linkedin.com/in/alexandre-ndanyuzwe-cyiza-766644210/">
-              <img
-                src="/images/icons8-linkedin-logo-48.png"
-                alt="LinkedIn"
-              />
+              <img src="/images/icons8-linkedin-logo-48.png" alt="LinkedIn" />
             </a>
           </div>
           <div>
