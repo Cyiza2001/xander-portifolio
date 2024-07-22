df6b336660fe1e0c10e31483914ef6c420a89ac5
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-07-22 13:13:06 +0200

    another commit

--- src/Achievements.jsx
+++ src/Achievements.jsx
@@ -12,17 +12,17 @@ const stats = [
 const Achievements = ()=>{
     return(
     <div>
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col  justify-center items-center">
       <div className="flex font-bold text-2xl mt-14 mb-6">STATS</div>
       <div className="flex " style={{width:"122.83px"}}>
-         <div className="flex justify-center items-center w-full rounded bg-gray-200 " style={{height:"2px"}}>
+         <div className="flex  justify-center items-center w-full rounded bg-gray-200 " style={{height:"2px"}}>
           <div className="h-1 rounded bg-blue-700  w-12"></div>
          </div>
         </div>
         <p className="flex  my-6 font-normal">Achievements & Statistics</p>
     </div>
           {/***********************************************STATS DIVS DOWN HERE************************************************************** */}
-          <div className="flex flex-row gap-40 justify-center items-center mt-3 ">
+          <div className="flex sm:flex-row flex-col sm:gap-36  gap-10 justify-center items-center mt-3 px-14">
             {stats.map((el)=>{
 
                 return(
