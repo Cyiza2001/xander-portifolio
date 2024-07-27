0da08b8a1be8851083e62d858d7d5b3023394738
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-07-27 22:14:36 +0200

    another commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       backgroundImage: {
         home: "url('https://images.pexels.com/photos/80476/hayden-valley-yellowstone-valley-landscape-80476.jpeg?cs=srgb&dl=landscape-nature-sky-80476.jpg&fm=jpg')",
       },
+      boxShadow: {
+        'custom-up': '0 -10px 20px rgba(0, 0, 0, 0.1), 0 -10px 20px rgba(0, 0, 0, 0.1)', // custom shadow pushing up
+      },
     },
   },
   plugins: [],
