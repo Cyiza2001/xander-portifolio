e17f3530499af43c0140a8548c5f1536cd7d3935
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-07-29 16:10:08 +0200

    another commit

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -28,16 +28,27 @@ const Testimonials = ()=>{
              <div className="flex mt-12 border-2 border-gray-200 overflow-hidden rounded-full h-16 w-16 "><img src={picture} alt=" Testimony Giver" className="object-cover h-full w-full" /></div>
              <p className="font-bold mt-2 text-lg " style={{marginBottom:"1px"}}>KALISA TCHAMI</p>
              <p className="text-gray-400 font-semibold text-base">Software Engineer at SevenX</p>
-            </div>
-            <div className="flex flex-wrap">
+
+
+             <div className="flex flex-row justify-center mx-52 mt-6">
             
-                <FaQuoteLeft className="text-blue-700" />
-               <p className="flex flex-row">I've seen firsthand how dedicated and skilled Cyiza is as a software developer. With expertise in technologies like React, Express.js, and MongoDB, they consistently deliver robust and innovative solutions. Their commitment to best practices and strong ethical values make them a standout professional in the field.</p>
-               <FaQuoteRight className="text-blue-700"  />
-        
-                
-         
+               <FaQuoteLeft className="text-blue-700 text-4xl mr-2" />
+               <p className="flex flex-row text-center" >I've seen firsthand how dedicated and skilled Cyiza is as a software developer. With expertise in technologies like React, Express.js, and MongoDB, they consistently deliver robust and innovative solutions. Their commitment to best practices and strong ethical values make them a standout professional in the field.</p>
+              <FaQuoteRight className="text-blue-700 text-4xl ml-2 "/>
+    
+            
+     
              </div>
+            </div>
+            <div className="flex flex-row gap-2 mt-16 justify-center">
+
+              <div className=" h-3 w-3 border-blue-600 border-2 rounded-full"></div>
+              <div className=" h-3 w-3 border-blue-600 border-2 rounded-full"></div>
+              <div className=" h-3 w-3 border-blue-600 border-2 rounded-full"></div>
+              <div className=" h-3 w-3 border-blue-600 border-2 rounded-full"></div>
+
+            </div>
+  
          
         </div>
         </div>
