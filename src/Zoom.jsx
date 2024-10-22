5190dbb436a4ad644b91c25f44a9be52dd2fa199
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-10-22 23:29:58 +0200

    another commit

---
+++ src/Zoom.jsx
@@ -0,0 +1,16 @@
+import cert from "images/certify.jpg"
+
+
+const Zoom = ()=>{
+
+    return(
+        <Main className="flex flex-row">
+            <section>
+                <img src={cert} alt="certificate" />
+            </section>
+            <section>
+            </section>
+
+        </Main>
+    )
+}
