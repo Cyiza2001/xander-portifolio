2bd0e22f6e7a6e324cef285843b5cedf179af382
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-11-03 15:59:19 +0200

    another commit

--- src/Zoom.jsx
+++ src/Zoom.jsx
@@ -1,50 +1,31 @@
-// import cert from "/images/certify.jpg"
-
-
-// const DetailedPopup = ({isOpen, onClose})=>{
-//     if(!isOpen) return null
-
-//     return(
-//         <Main className="relative inset-0 flex flex-row z-30 w-full h-full bg-black">
-//             <button className="absolute top-2 right-2" onClick={onClose}>X</button>
-//             <section>
-//                 <img src={cert} alt="certificate" />
-//             </section>
-//             <section>
-//                 ggggggggggggggggggggggggggggggggggggggggggggg
-//             </section>
-
-//         </Main>
-//     )
-// }
- 
-// export default DetailedPopup;
-
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import cert from "/images/certify.jpg";
 import { ImCross } from "react-icons/im";
 
 const DetailedPopup = ({ isOpen, onClose }) => {
   useEffect(() => {
-    if (isOpen) document.body.style.overflow = 'hidden';
-    else document.body.style.overflow ='';
-    
+    if (isOpen) document.body.style.overflow = "hidden";
+    else document.body.style.overflow = "";
+
     // Clean up on unmount
-    return () =>  document.body.style.overflow = '';
+    return () => (document.body.style.overflow = "");
   }, [isOpen]);
 
   if (!isOpen) return null;
 
   return (
-    <div className="fixed  max-w-full h-full top-8 bottom-0 right-20 left-20  z-50 bg-black border-8">
-      <div className="relative flex flex-row align-middle ">
-        <button className="absolute top-2 right-2 text-xl" onClick={onClose}>
-        <ImCross />
+    <div className="fixed  max-w-full h-[calc(100vh-4rem)] top-8 bottom-0 right-20 left-20  z-50  border-8 overflow-y-scroll overflow-hidden">
+      <div className="relative flex flex-row align-middle h-full">
+        <button
+          className="absolute -top-2 -right-10 rounded-md text-xl bg-black p-2 hover:pointer hover:font-thin"
+          onClick={onClose}
+        >
+          <ImCross className=" text-white font-semibold " />
         </button>
-        <section className="w-2/3 h-auto">
-          <img src={cert} alt="certificate"  />
+        <section className="w-2/3 h-full ">
+          <img src={cert} alt="certificate" className="w-full h-full " />
         </section>
-        <section className=" w-1/3 h-28 text-white">
+        <section className=" w-1/3 h-28 text-black">
           Full certificate description or any additional content here.
         </section>
       </div>
